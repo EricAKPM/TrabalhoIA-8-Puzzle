d191ac80b080b1dbce2a35392562c267072000b5
EricAKPM <ericmili.m7@gmail.com>
2022-07-27 09:08:35 -0300

    m

---
+++ index.py
@@ -0,0 +1,212 @@
+import random
+import copy
+
+META = [[1,2,3],[4,5,6],[7,8,9]]
+
+class Noh:
+    def __init__(self, estado, pai, g, h):
+        self.estado = estado
+        self.pai = pai
+        self.g = g
+        self.h = h
+
+        
+    def localizar(self, atual, elemento):
+        for i in range(3):
+            for j in range(3):
+                if atual[i][j] == elemento:
+                    return i, j
+        
+
+    def moverAcima(self, atual):
+        linha, coluna = self.localizar(atual,9)
+        if linha - 1 >= 0:
+            atual[linha-1][coluna], atual[linha][coluna] = atual[linha][coluna], atual[linha-1][coluna]
+        return atual
+    def moverAbaixo(self, atual):
+        linha, coluna = self.localizar(atual,9)
+        if linha + 1 <= 2:
+            atual[linha+1][coluna], atual[linha][coluna] = atual[linha][coluna], atual[linha+1][coluna]
+        return atual
+    
+    def moverEsquerda(self, atual):
+        linha, coluna = self.localizar(atual,9)
+        if coluna - 1 >= 0:
+            atual[linha][coluna-1], atual[linha][coluna] = atual[linha][coluna], atual[linha][coluna-1]
+        return atual
+    
+    def moverDireita(self, atual):
+        linha, coluna = self.localizar(atual, 9)
+        if coluna + 1 <= 2:
+            atual[linha][coluna+1], atual[linha][coluna] = atual[linha][coluna], atual[linha][coluna+1]
+        return atual
+
+
+
+class Puzzle:
+    def criarNo(self, atual, pai, g):
+        h = g + self.calcularHeuristicaManhattan(copy.deepcopy(atual))
+        return Noh(atual, pai, g, h)
+
+    def geraTabuleiro(self):
+        tabela = [[0 for i in range(3)] for j in range(3)]
+        aleatorio = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        for x in range(0, 3):
+            for y in range(0, 3):
+                z = random.randrange(0, len(aleatorio))
+                tabela[x][y] = aleatorio[z]
+                aleatorio.pop(z)
+        return tabela
+
+    def solucionavel(self, atual):
+        contador=0
+        for i in range(0,3):
+            for e in range(0,3):
+                if atual.estado[i][e] == 9:
+                    continue
+                for j in range(i, 3):
+                    for k in range(0, 3):
+                        if atual.estado[j][k]==9 or (k < e and i==j):
+                            continue
+                        if atual.estado[j][k] > atual.estado[i][e]:
+                            contador+=1
+        if (contador % 2 == 1 ):
+            return False
+        else:
+            return True
+    
+    def calcularHeuristicaManhattan(self, atual):
+        contador = 0
+        for x in range(0, 3):
+            for y in range(0,3):
+                for x1 in range(0,3):
+                    for x2 in range(0,3):
+                        if(atual[x][y]==META[x1][x2]):
+                            posY = x2
+                            posX = x1
+                posIdealX = abs(y - posY)
+                posIdealY = abs(x - posX)
+                contador = contador + posIdealX + posIdealY
+        return contador
+ 
+    def __init__(self):
+        gerado = self.geraTabuleiro()
+        tabuleiro = self.criarNo(gerado, None, 0)
+        #tabuleiro = self.criarNo([[1,2,8],[5,4,9],[3,6,7]], None, 0)
+        if(self.solucionavel(tabuleiro)):
+            #self.AStar(tabuleiro)
+            self.AStarProfundidade(tabuleiro)
+        else:
+            print("Impossivel!!")
+    
+    def imprimir(self,noh, n):
+        print("MOVIMENTOS: " + str(n))
+        print(noh.estado)
+        print("\n\n")
+
+    #LARGURA
+    def sucessoresPossiveis(self, noh):
+        estado = noh.estado
+        """pai = noh.pai
+        if pai:
+            estadoPai = pai.estado
+        else:
+            estadoPai = None"""
+        lista = []
+        l1 = noh.moverAcima(copy.deepcopy(estado))
+        if l1 != noh.estado:
+            lista.append(l1)
+        l2 = noh.moverAbaixo(copy.deepcopy(estado))
+        if l2 != noh.estado:
+            lista.append(l2)
+        l3 = noh.moverEsquerda(copy.deepcopy(estado))
+        if l3 != noh.estado:
+            lista.append(l3)
+        l4 = noh.moverDireita(copy.deepcopy(estado))
+        if l4 != noh.estado:
+            lista.append(l4)
+        return lista
+
+    def inserirNo(self, noh, aberta, fechada):
+        for x in aberta:
+            if x.estado == noh.estado:
+                return aberta
+        for x in fechada:
+            if x == noh.estado:
+                return aberta 
+        aberta.append(noh)
+        aberta.sort(key=lambda x : x.g)
+        return aberta
+
+    def AStar(self, atual):
+        NumeroMovimentos = 0
+        aberta = []
+        fechada = []
+        aberta.append(atual)
+        while aberta:
+            noh = aberta[0]
+            aberta.pop(0)
+            self.imprimir(noh, NumeroMovimentos)
+            if noh.estado == META:
+                print("META atigida com: " + str(NumeroMovimentos) + " movimentos")
+                return
+            NumeroMovimentos+=1
+            sucessores = self.sucessoresPossiveis(noh)
+            for x in sucessores:
+                self.inserirNo(self.criarNo(x, noh, noh.g), aberta, fechada)
+            if noh.estado not in fechada:
+                fechada.append(copy.deepcopy(noh.estado))
+
+    #PROFUNDIDADE
+    def sucessoresPossiveisProfundidade(self, noh):
+        estado = noh.estado
+
+        lista = []
+        l1 = noh.moverAcima(copy.deepcopy(estado))
+        if l1 != noh.estado:
+            lista.append(l1)
+        l2 = noh.moverAbaixo(copy.deepcopy(estado))
+        if l2 != noh.estado:
+            lista.append(l2)
+        l3 = noh.moverEsquerda(copy.deepcopy(estado))
+        if l3 != noh.estado:
+            lista.append(l3)
+        l4 = noh.moverDireita(copy.deepcopy(estado))
+        if l4 != noh.estado:
+            lista.append(l4)
+        for x in range(0, len(lista)):
+            for y in range(x, len(lista)):
+                if self.calcularHeuristicaManhattan(lista[x]) < self.calcularHeuristicaManhattan(lista[y]):
+                    lista[x],lista[y]=lista[y],lista[x]
+        return lista
+    
+    def inserirNoProfundidade(self, noh, aberta, fechada):
+        for x in aberta:
+            if x.estado == noh.estado:
+                return aberta
+        for x in fechada:
+            if x == noh.estado:
+                return aberta
+        aberta.insert(0,noh)
+        return aberta
+    
+    def AStarProfundidade(self, atual):
+        NumeroMovimentos = 0
+        aberta = []
+        fechada = []
+        aberta.append(atual)
+        while aberta:
+            noh = aberta[0]
+            aberta.pop(0)
+            self.imprimir(noh, NumeroMovimentos)
+            if noh.estado == META:
+                print("META atigida com: " + str(NumeroMovimentos) + " movimentos")
+                return
+            NumeroMovimentos+=1
+            sucessores = self.sucessoresPossiveisProfundidade(noh)
+            for x in sucessores:
+                self.inserirNoProfundidade(self.criarNo(x, noh, noh.g), aberta, fechada)
+            if noh.estado not in fechada:
+                fechada.append(copy.deepcopy(noh.estado))
+
+puzzle = Puzzle()
--- trab.py
+++ trab.py
@@ -1,72 +1,117 @@
-import math
 import random
+import sys
 
-class ordem:
+sys.setrecursionlimit(3000)
+
+class ordem: #classe que é usada para definir a prioridade na fila
     def __init__(self, val, dire):
         self.valor = val
         self.direcao = dire
     def saida(self):
         print(self.valor, self.direcao)
 
-def checarciclo(atual):
-    print('testando ciclos')
+def imprimirTabela(atual):
+    for a in atual:
+        for b in a:
+            print("|", b, end=" ")
+        print("\n-------------")
+    print("\n\n\n")
+
+def checarciclo(atual): #checa se a tabela fecha um ciclo
+    """print('IMPRIMINDO CONTEUDO DE CICLO:')
     for x in ciclo:
+        print(x)
+    print('FIM  IMPRIMINDO CONTEUDO DE CICLO')
+    print('testando ciclos')"""
+    for x in ciclo:
+        #print(str(atual) + "  = " + str(x))
         if(atual == x):
-            return False
-    return True
+            #print('CICLO')
+            return True
+    #print('fim testando ciclos\n\n\n\n')
+    return False
 
-ciclo=[]
+flag = False #Flag para descobrir se a meta foi alcancada
+ciclo=[] #Fila que guarda os estados da arvore para nao ocorrer ciclos
+meta = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] #Meta
 
-def manhattan(tabela, x, y):
+def manhattan(tabela, x, y, g):
+    imprimirTabela(tabela)
+    global flag
     
-    if aleatorio == tabela and checarciclo(tabela):
+    if meta == tabela:
+        flag = True
         return
 
-    print('TABELA: ', tabela)
+    if checarciclo(tabela) == True or flag == True:
+        return
 
-    ciclo.append(tabela)
+    moverDireita = [[0 for i in range(3)] for j in range(3)]
+    moverEsquerda= [[0 for i in range(3)] for j in range(3)]
+    moverCima= [[0 for i in range(3)] for j in range(3)]
+    moverBaixo = [[0 for i in range(3)] for j in range(3)]
 
+    for a in range(0,3):
+        for b in range(0,3):
+            moverEsquerda[a][b]=tabela[a][b]
+            moverDireita[a][b]=tabela[a][b]
+            moverCima[a][b]=tabela[a][b]
+            moverBaixo[a][b]=tabela[a][b]
+
+    ciclo.append(tabela.copy())
     ordenacao = []
+
+
     if x - 1 >= 0:
-        ordenacao.append(ordem(calcularHeuristicaManhattan(), "esquerda"))
+        aux=moverCima[x][y]
+        moverCima[x][y]=moverCima[x-1][y]
+        moverCima[x-1][y]=aux
+        ordenacao.append(ordem(calcularHeuristicaManhattan(moverCima)+g, "cima"))
+    else:
+        del moverCima
     if x + 1 <= 2:
-        ordenacao.append(ordem(calcularHeuristicaManhattan(), "direita"))
+        ordenacao.append(ordem(calcularHeuristicaManhattan(moverBaixo)+g, "baixo"))
+        aux=moverBaixo[x][y]
+        moverBaixo[x][y]=moverBaixo[x+1][y]
+        moverBaixo[x+1][y]=aux
+    else:
+        del moverBaixo
     if y - 1 >= 0:
-        ordenacao.append(ordem(calcularHeuristicaManhattan(), "cima"))
+        aux=moverEsquerda[x][y]
+        moverEsquerda[x][y]=moverEsquerda[x][y-1]
+        moverEsquerda[x][y-1]=aux
+        ordenacao.append(ordem(calcularHeuristicaManhattan(moverEsquerda)+g, "esquerda"))
+    else:
+        del moverEsquerda
     if y + 1 <= 2:
-        ordenacao.append(ordem(calcularHeuristicaManhattan(), "baixo"))
+        aux=moverDireita[x][y]
+        moverDireita[x][y]=moverDireita[x][y+1]
+        moverDireita[x][y+1]=aux
+        ordenacao.append(ordem(calcularHeuristicaManhattan(moverDireita)+g, "direita"))
+    else:
+        del moverDireita
     ordenacao.sort(key =lambda x : x.valor)
-    #for obj in ordenacao:
-    if ordenacao[0].direcao == "esquerda":
-        aux=tabela[x][y]
-        tabela[x][y]=tabela[x-1][y]
-        tabela[x-1][y]=aux
-        manhattan(tabela, x-1, y)
-    elif ordenacao[0].direcao == "direita":
-        aux=tabela[x][y]
-        tabela[x][y]=tabela[x+1][y]
-        tabela[x+1][y]=aux
-        manhattan(tabela, x+1, y)
-    elif ordenacao[0].direcao == "cima":
-        aux=tabela[x][y]
-        tabela[x][y]=tabela[x][y-1]
-        tabela[x][y-1]=aux
-        manhattan(tabela, x, y-1)
-    elif ordenacao[0].direcao == "baixo":
-        aux=tabela[x][y]
-        tabela[x][y]=tabela[x][y+1]
-        tabela[x][y+1]=aux
-        manhattan(tabela, x, y+1)
 
-        
+    for obj in ordenacao:
+        if obj.direcao == "cima":
+            manhattan(moverCima.copy(), x-1, y, obj.valor)
+        elif obj.direcao == "baixo":
+            manhattan(moverBaixo.copy(), x+1, y, obj.valor)
+        elif obj.direcao == "esquerda":
+            manhattan(moverEsquerda.copy(), x, y-1, obj.valor)
+        elif obj.direcao == "direita":
+            manhattan(moverDireita.copy(), x, y+1, obj.valor)
+        if flag == True:
+            return
+    ciclo.pop(len(ciclo)-1)
 
-def calcularHeuristicaManhattan():
+def calcularHeuristicaManhattan(atual):
     contador = 0
     for x in range(0, 3):
         for y in range(0,3):
             for x1 in range(0,3):
                 for x2 in range(0,3):
-                    if(tabela[x][y]==aleatorio[x1][x2]):
+                    if(atual[x][y]==meta[x1][x2]):
                         posY = x2
                         posX = x1
             posIdealX = abs(y - posY )
@@ -74,6 +119,77 @@ def calcularHeuristicaManhattan():
             contador = contador + posIdealX + posIdealY
     return contador
 
+def profundidade(tabela, x, y, recursao):
+    print(recursao, x, y)
+    imprimirTabela(tabela)
+    global flag
+    
+    if meta == tabela:
+        flag = True
+        return
+
+    if checarciclo(tabela) == True or flag == True:
+        return
+
+
+    ciclo.append(tabela.copy())
+    mover = [[0 for i in range(3)] for j in range(3)]
+
+    for a in range(0,3):
+        for b in range(0,3):
+            mover[a][b]=tabela[a][b]
+
+    if x - 1 >= 0:
+        aux=mover[x][y]
+        mover[x][y]=mover[x-1][y]
+        mover[x-1][y]=aux
+        profundidade(mover.copy(), x-1, y, recursao+1)
+
+    for a in range(0,3):
+        for b in range(0,3):
+            mover[a][b]=tabela[a][b]
+
+    if x + 1 <= 2:
+        aux=mover[x][y]
+        mover[x][y]=mover[x+1][y]
+        mover[x+1][y]=aux
+        profundidade(mover.copy(), x+1, y, recursao+1)
+
+    for a in range(0,3):
+        for b in range(0,3):
+            mover[a][b]=tabela[a][b]
+
+    if y - 1 >= 0:
+        aux=mover[x][y]
+        mover[x][y]=mover[x][y-1]
+        mover[x][y-1]=aux
+        profundidade(mover.copy(), x, y-1, recursao+1)
+
+    for a in range(0,3):
+        for b in range(0,3):
+            mover[a][b]=tabela[a][b]
+
+    if y + 1 <= 2:
+        aux=mover[x][y]
+        mover[x][y]=mover[x][y+1]
+        mover[x][y+1]=aux
+        profundidade(mover.copy(), x, y+1, recursao+1)
+    ciclo.pop(len(ciclo)-1)
+     
+def solucionavel(atual):
+    contador=0
+    for x in range(0, 3):
+        for y in range(0, 3):
+            if(tabela[x][y]!=9):
+                for z in range(x, 3):
+                    for a in range(y, 3):
+                        if(atual[x][y] > atual[z][a]):
+                            contador=contador+1
+    print('contador: ', contador)
+    if (contador % 2 == 1 ):
+        return False
+    else:
+        return True
 
 tabela = [[0 for i in range(3)] for j in range(3)]
 aleatorio = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -83,10 +199,16 @@ for x in range(0, 3):
         tabela[x][y] = aleatorio[z]
         aleatorio.pop(z)
 
-tabela = [[7, 2, 4], [5, 9, 6], [8, 3, 1]]
+for x in range(0, 3):
+    for y in range(0, 3):
+        if(tabela[x][y] == 9):
+            p1=x
+            p2=y
 
-# Se torna a meta
-aleatorio = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-# print(tabela)
-# print(calcularHeuristicaManhattan())
-manhattan(tabela, 0, 0)
+#tabela = [[1,2,3],[4,5,6],[7,9,8]]
+if(solucionavel(tabela)):#verificar paridade
+    #manhattan(tabela.copy(), 2, 1,0)
+    manhattan(tabela.copy(), p1, p2,0)
+    #profundidade(tabela.copy(), 2, 1, 0)
+else:
+    print('TABELA IMPOSSIVEL!') 
